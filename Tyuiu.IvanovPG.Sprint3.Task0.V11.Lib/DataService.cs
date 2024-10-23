fe3d19dff836b33fc47ceabf0c10f8ee8fc0779c
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-10-23 18:21:29 +0500

    Task0

--- Tyuiu.IvanovPG.Sprint3.Task0.V11.Lib/DataService.cs
+++ Tyuiu.IvanovPG.Sprint3.Task0.V11.Lib/DataService.cs
@@ -16,7 +16,7 @@ using tyuiu.cources.programming.interfaces.Sprint3;
         return Math.Round(sumSeries, 3);
     }
 }
-}
+
 //Math.Pow((1 / Math.Pow(n, k)), 2)
 //sumSeries = sumSeries + ((1 / Math.Sin(i)) + value);
 //Math.Pow( (1 / Math.Pow(value, k)), 2)

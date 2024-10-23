9c31cb14ed8f9e54d731b011fa0a4e25ed6fddd1
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-10-23 19:04:06 +0500

    Task3

---
+++ Tyuiu.IvanovPG.Sprint3.Task3.V29.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.IvanovPG.Sprint3.Task3.V29.Lib
+{
+    public class DataService : ISprint3Task3V29
+    {
+        public string DeleteCharInString(string value, char item)
+        {
+            string delString = "";
+
+            foreach (char h in value)
+            {
+                if (h == item) continue;
+                else delString += h;
+            }
+            return delString;
+        }
+    }
+}
+//public double GetSumSeries(int startValue, int stopValue)
+//{
+//    double sumSeries = 0;
+//    do
+//    {
+//        sumSeries = sumSeries + (Math.Sin(startValue) * (Math.Pow((0.5), 2)));
+//        startValue++;
+//    } while (startValue <= stopValue);
+//    return Math.Round(sumSeries, 3);
+//}
---
+++ Tyuiu.IvanovPG.Sprint3.Task3.V29/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.IvanovPG.Sprint3.Task3.V29.Lib;
+
+namespace Tyuiu.IvanovPG.Sprint3.Task3.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# ИСХОДНЫЕ ДАННЫЕ:                                                        #");
+            Console.WriteLine("###########################################################################");
+
+            string value = "chgr vhhtg hnht";
+            char item = 'h';
+
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Удаляемый символ = " + item);
+
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# РЕЗУЛЬТАТ:                                                              #");
+            Console.WriteLine("###########################################################################");
+
+            Console.WriteLine("Преобразованная строка = " + ds.DeleteCharInString(value, item));
+            Console.ReadKey();
+
+        }
+    }
+}
